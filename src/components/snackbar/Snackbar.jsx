3f05437f4d8557b070f19bfe229ba4e5972043c1
hemant.gogoi <hemant63821@gmail.com>
2023-12-12 21:55:16 +0530

    fix() : login page layout

---
+++ src/components/snackbar/Snackbar.jsx
@@ -0,0 +1,25 @@
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+export default function SnackBarNotification(props) {
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={props.openNotification}
+        onClose={props.closeSnackbar}
+        key={"top" + "right"}
+        autoHideDuration={10000}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity={props.severity || "error"}
+          onClose={props.closeSnackbar}
+        >
+          {props.message}
+        </MuiAlert>
+      </Snackbar>
+    </div>
+  );
+}
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,87 @@
+import { Button, makeStyles } from "@material-ui/core";
+import React, { Fragment } from "react";
+import SnackBarNotification from "../components/snackbar/Snackbar";
 import "./Login.scss";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+  subHeader: {
+    color: "#66759c",
+    fontSize: "14px",
+  },
+}));
+
 export default function Login(props) {
+  const classes = useStyles();
+  const [snackBar, openSnackBar] = React.useState(false);
+  const [notificationMsg, setNotifificationMsg] = React.useState({
+    msg: "",
+    severity: "error",
+  });
+  const [verifyOtp, openVerifyOtp] = React.useState(false);
+
+  const authenticate = () => {};
+
+  const openForgotTab = () => {
+    openVerifyOtp(true);
+  };
+
   return (
-    <div>
-      {console.log("check here if navigated")}
-      <div>check 1 2 3</div>
-      chehck and test 123
+    <div className="login_container container-fluid">
+      <SnackBarNotification
+        openNotification={"test"}
+        closeSnackbar={() => {
+          openSnackBar(false);
+        }}
+        severity={notificationMsg.severity}
+        message={notificationMsg.msg}
+      />
+
+      <div className="login-card">
+        <div className="login-logo mt-4">
+          <img
+            alt="login_logo"
+            className="logo"
+            src="images/easygenerator.jpeg"
+          />
+        </div>
+        <div className="content mt-5">
+          <form
+            className={classes.root}
+            noValidate
+            autoComplete="current-password"
+          ></form>
+        </div>
+        <Fragment>
+          <div className="footer mt-4">
+            <Button
+              variant="contained"
+              className="login-button"
+              color="secondary"
+              onClick={authenticate}
+            >
+              Login
+            </Button>
+          </div>
+          <div className="footer mt-4">
+            <span
+              onClick={openForgotTab}
+              style={{
+                cursor: "pointer",
+                color: "#66759c",
+                textDecoration: "underline",
+              }}
+            >
+              Forgot Password?
+            </span>
+          </div>
+        </Fragment>
+      </div>
     </div>
   );
 }
