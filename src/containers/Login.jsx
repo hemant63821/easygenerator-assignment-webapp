87496056ff9dfdb58463abd6a51bc73592e86a3b
hemant.gogoi <hemant63821@gmail.com>
2023-12-13 00:10:03 +0530

    feature() : login page feature without api integration

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,12 @@
-import { Button, makeStyles } from "@material-ui/core";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import React, { Fragment } from "react";
 import SnackBarNotification from "../components/snackbar/Snackbar";
 import "./Login.scss";
@@ -13,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
   subHeader: {
     color: "#66759c",
     fontSize: "14px",
+    marginLeft: "15px",
   },
 }));
 
@@ -23,12 +32,112 @@ export default function Login(props) {
     msg: "",
     severity: "error",
   });
-  const [verifyOtp, openVerifyOtp] = React.useState(false);
+  const [signupScreen, openSignUpScreen] = React.useState(false);
+  const [password, setPassword] = React.useState("");
+  const [pwdError, setPwdError] = React.useState({
+    error: false,
+    errorType: "",
+  });
+  const [showPwd, setShowPwd] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [nameError, setNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState({
+    error: false,
+    errorType: "",
+  });
 
   const authenticate = () => {};
 
-  const openForgotTab = () => {
-    openVerifyOtp(true);
+  const passwordValidator = (pwd) => {
+    const validation = {
+      error: false,
+      errorType: "",
+    };
+
+    if (!pwd) {
+      validation.error = true;
+      validation.errorType = "Password Required!";
+      return validation;
+    }
+    const minLength = pwd?.length < 8 ? false : true;
+    const hasLetter = pwd.match(/[a-zA-Z]/);
+    const hasNumber = pwd.match(/\d/);
+    const hasSpecialChar = pwd.match(/[!@#$%^&*(),.?":{}|<>]/);
+
+    if (!minLength) {
+      validation.error = true;
+      validation.errorType = "minimum 8 characters required!";
+    }
+
+    if (!hasLetter) {
+      validation.error = true;
+      validation.errorType = "Atleast one character required [a-zA-Z]!";
+    }
+
+    if (!hasNumber) {
+      validation.error = true;
+      validation.errorType = "Atleast one number required 0-9!";
+    }
+
+    if (!hasSpecialChar) {
+      validation.error = true;
+      validation.errorType =
+        "atleast one special character required !@#$%^&*(),.?:{}|<></>";
+    }
+
+    return validation;
+  };
+
+  const emailValidator = (email) => {
+    const validation = {
+      error: false,
+      errorType: "",
+    };
+
+    if (!email) {
+      validation.error = true;
+      validation.errorType = "Email Required!";
+      return validation;
+    }
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+    if (!email.match(emailRegex)) {
+      validation.error = true;
+      validation.errorType = "Invalid Email Format!";
+    }
+
+    return validation;
+  };
+
+  const validateAndSetInputFields = (value, type) => {
+    if (type === "name") {
+      setName(value);
+      value ? setNameError(false) : setNameError(true);
+    }
+    if (type === "pwd") {
+      setPassword(value);
+      const ispasswordValidated = passwordValidator(value);
+      setPwdError(ispasswordValidated);
+    }
+    if (type === "email") {
+      setEmail(value);
+      const isEmailValidated = emailValidator(value);
+      setEmailError(isEmailValidated);
+    }
+  };
+
+  const resetStates = () => {
+    setName("");
+    setEmailError({
+      error: false,
+      errorType: "",
+    });
+    setNameError(false);
+    setPwdError({
+      error: false,
+      errorType: "",
+    });
   };
 
   return (
@@ -43,43 +152,196 @@ export default function Login(props) {
       />
 
       <div className="login-card">
-        <div className="login-logo mt-4">
+        <div className="login-logo">
           <img
             alt="login_logo"
             className="logo"
             src="images/easygenerator.jpeg"
           />
         </div>
-        <div className="content mt-5">
+        <div className="content">
           <form
             className={classes.root}
             noValidate
             autoComplete="current-password"
-          ></form>
+          >
+            <Fragment>
+              {signupScreen ? (
+                <Fragment>
+                  <div>
+                    <TextField
+                      error={nameError}
+                      id="outlined-error-helper-text-otpEmail"
+                      label="Name"
+                      helperText={nameError ? "Name Required." : ""}
+                      variant="outlined"
+                      fullWidth
+                      value={name}
+                      style={{ width: 350 }}
+                      onChange={(e) =>
+                        validateAndSetInputFields(e.target.value, "name")
+                      }
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={emailError.error}
+                      id="outlined-error-helper-text-otpEmail"
+                      label="Email"
+                      helperText={emailError.error ? emailError.errorType : ""}
+                      variant="outlined"
+                      fullWidth
+                      value={email}
+                      style={{ width: 350 }}
+                      onChange={(e) =>
+                        validateAndSetInputFields(e.target.value, "email")
+                      }
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={pwdError.error}
+                      id="current-password"
+                      label="password"
+                      type={showPwd ? null : "password"}
+                      helperText={pwdError.error ? pwdError.errorType : ""}
+                      variant="outlined"
+                      fullWidth
+                      value={password}
+                      style={{ width: 350 }}
+                      onChange={(e) =>
+                        validateAndSetInputFields(e.target.value, "pwd")
+                      }
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={() => {
+                                setShowPwd((itr) => !itr);
+                              }}
+                            >
+                              {showPwd ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <div>
+                    <TextField
+                      error={emailError.error}
+                      id="outlined-error-helper-text-otpEmail"
+                      label="Email"
+                      helperText={emailError.error ? emailError.errorType : ""}
+                      variant="outlined"
+                      fullWidth
+                      value={email}
+                      style={{ width: 350 }}
+                      onChange={(e) =>
+                        validateAndSetInputFields(e.target.value, "email")
+                      }
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={pwdError.error}
+                      id="current-password"
+                      label="password"
+                      type={showPwd ? null : "password"}
+                      helperText={pwdError.error ? pwdError.errorType : ""}
+                      variant="outlined"
+                      fullWidth
+                      value={password}
+                      style={{ width: 350 }}
+                      onChange={(e) =>
+                        validateAndSetInputFields(e.target.value, "pwd")
+                      }
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={() => {
+                                setShowPwd((itr) => !itr);
+                              }}
+                            >
+                              {showPwd ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+                </Fragment>
+              )}
+            </Fragment>
+          </form>
         </div>
         <Fragment>
-          <div className="footer mt-4">
-            <Button
-              variant="contained"
-              className="login-button"
-              color="secondary"
-              onClick={authenticate}
-            >
-              Login
-            </Button>
-          </div>
-          <div className="footer mt-4">
-            <span
-              onClick={openForgotTab}
-              style={{
-                cursor: "pointer",
-                color: "#66759c",
-                textDecoration: "underline",
-              }}
-            >
-              Forgot Password?
-            </span>
-          </div>
+          {signupScreen ? (
+            <div className="footer">
+              <Button
+                variant="contained"
+                className="login-button"
+                style={{ width: "100%" }}
+                color="secondary"
+                onClick={authenticate}
+              >
+                Create
+              </Button>
+
+              <div class="break-line"></div>
+
+              <Button
+                variant="contained"
+                className="signup-button"
+                style={{ width: "50%" }}
+                color="secondary"
+                startIcon={<ArrowBackIcon />}
+                onClick={() => {
+                  openSignUpScreen(false);
+                  resetStates();
+                }}
+              >
+                Goback
+              </Button>
+            </div>
+          ) : (
+            <div className="footer">
+              <Button
+                variant="contained"
+                className="login-button"
+                style={{ width: "100%" }}
+                color="secondary"
+                onClick={authenticate}
+              >
+                Login
+              </Button>
+
+              <div class="break-line"></div>
+
+              <Button
+                variant="contained"
+                className="signup-button"
+                style={{ width: "50%" }}
+                color="secondary"
+                onClick={() => {
+                  openSignUpScreen(true);
+                  setPassword("");
+                  resetStates();
+                }}
+              >
+                Create account
+              </Button>
+            </div>
+          )}
         </Fragment>
       </div>
     </div>
--- src/helpers/store.jsx
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import { createLogger } from "redux-logger";
-import rootReducer from "../redux/reducers/index";
-
-const loggerMiddleware = createLogger();
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
-
-export default store;
