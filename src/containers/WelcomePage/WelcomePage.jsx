03f4e07749c759a6b2140bf82dcb7638a1475592
hemant.gogoi <hemant63821@gmail.com>
2023-12-13 02:38:00 +0530

    fix() : get user data from local storage and display on welcome page

--- src/containers/WelcomePage/WelcomePage.jsx
+++ src/containers/WelcomePage/WelcomePage.jsx
@@ -1,13 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { getLoginUser } from "../../helpers/storage";
 import "./WelcomePage.scss";
 
 export default function WelcomePage(props) {
-  const [userName, setUserName] = React.useState("");
+  const [userName, setUserName] = React.useState(null);
+
+  useEffect(() => {
+    const data = getLoginUser();
+    if (data) {
+      setUserName(data?.userName || "");
+    }
+  }, []);
+
   return (
     <Fragment>
       <div className="welcome_screen">
         <div className="welcom_screen_content">
-          <h1>Welcome {userName} to the Application</h1>
+          <h1>Welcome {userName} to this Application</h1>
         </div>
       </div>
     </Fragment>
